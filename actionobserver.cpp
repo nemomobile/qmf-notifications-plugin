598c3b7c3efa49c34f546116055d7bc354aff33a
Valerio Valerio <valerio.valerio@jollamobile.com>
2013-06-24 07:18:45 +0300

    [qmf-notifications-plugin] Notifications for new emails.

--- actionobserver.cpp
+++ actionobserver.cpp
@@ -1,5 +1,37 @@
-#include "actionobserver.h"
+/*
+ * Copyright (C) 2013 Jolla Ltd.
+ * Contact: Valerio Valerio <valerio.valerio@jollamobile.com>
+ *
+ * You may use this file under the terms of the BSD license as follows:
+ *
+ * "Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Nemo Mobile nor the names of its contributors
+ *     may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+ */
 
+#include "actionobserver.h"
+#include <QtGlobal>
 #include <QDebug>
 
 // Utility function for debug proposes
@@ -71,9 +103,12 @@ RunningAction::RunningAction(QSharedPointer<QMailActionInfo> action,
     _transferClient(new TransferEngineClient(this))
 {
     _progress = 0.0;
-    connect (_action.data(), SIGNAL(activityChanged(QMailServiceAction::Activity)), this, SLOT(activityChanged(QMailServiceAction::Activity)));
-    connect (_action.data(), SIGNAL(statusAccountIdChanged(const QMailAccountId&)), this, SLOT(statusAccountIdChanged(const QMailAccountId&)));
-    connect (_action.data(), SIGNAL(progressChanged(uint, uint)), this, SLOT(progressChanged(uint, uint)));
+    connect(_action.data(), SIGNAL(activityChanged(QMailServiceAction::Activity)),
+            this, SLOT(activityChanged(QMailServiceAction::Activity)));
+    connect(_action.data(), SIGNAL(statusAccountIdChanged(const QMailAccountId&)),
+            this, SLOT(statusAccountIdChanged(const QMailAccountId&)));
+    connect(_action.data(), SIGNAL(progressChanged(uint, uint)),
+            this, SLOT(progressChanged(uint, uint)));
 }
 
 void RunningAction::activityChanged(QMailServiceAction::Activity activity)
@@ -101,7 +136,7 @@ void RunningAction::activityChanged(QMailServiceAction::Activity activity)
 void RunningAction::progressChanged(uint value, uint total)
 {
     if (value < total) {
-        qreal percent = qBound(0.0, (qreal)value / (qreal)total, 1.0);
+        qreal percent = qBound<qreal>(0.0, (qreal)value / total, 1.0);
         // Avoid spamming transfer-ui
         if (percent > _progress + 0.05 || percent == 1) {
             _progress = percent;
@@ -114,7 +149,7 @@ void RunningAction::progressChanged(uint value, uint total)
 
 void RunningAction::statusAccountIdChanged(const QMailAccountId &accountId)
 {
-    Q_ASSERT(accountId.toULongLong() <= 0);
+    Q_ASSERT(accountId.isValid());
     if (!_runningInTransferEngine) {
         AccountInfo acctInfo = _accountsCache->accountInfo(static_cast<Accounts::AccountId>(accountId.toULongLong()));
         _transferId = _transferClient->createSyncEvent(acctInfo.name, QUrl(), acctInfo.providerIcon);
@@ -166,14 +201,20 @@ void ActionObserver::actionsChanged(QList<QSharedPointer<QMailActionInfo> > acti
             connect(runningAction, SIGNAL(actionComplete(quint64)), this, SLOT(actionCompleted(quint64)));
         }
     }
-    // Sometimes actionsChanged signals comes too late still containing actions that are already completed
-    if (actionsList.size() == 0 && _completedActions.size() > 0)
-        _completedActions.clear();
+
+    if (actionsList.size() == 0) {
+        // Sometimes actionsChanged signals comes too late still containing actions that are already completed
+         if (_completedActions.size() > 0) {
+             _completedActions.clear();
+         }
+         // No more actions running
+         emit actionsCompleted();
+    }
 }
 
 void ActionObserver::actionCompleted(quint64 id)
 {
-    Q_ASSERT(!_runningActions.contains(id));
+    Q_ASSERT(_runningActions.contains(id));
     _runningActions.remove(id);
     _completedActions.append(id);
 }
